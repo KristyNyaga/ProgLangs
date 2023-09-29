06004167fe954a8e5e1f4032125a01deb4a8bb95
KristyNyaga <123406720+KristyNyaga@users.noreply.github.com>
2023-09-29 13:27:44 +0300

    added simple calculator assignment

---
+++ Simple Calculator/sumcalculator.py
@@ -0,0 +1,12 @@
+#NYAGA JOY CHRISTINE MUTHONI
+#SCT211-0572/2022
+print("Enter your name>>")
+username = input()
+print("Hello, " + username)
+print("This is a simple calculator that adds 2 numbers.")
+print("Enter 2 integers you would like to add>>")
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
+#print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(sum) )
